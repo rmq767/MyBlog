d5b9656130705ed01b28248c8899e503a0f65346
rmq767 <2501706076@qq.com>
2020-03-16 20:40:18 +0800

    完成主页搭建

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,117 +1,126 @@
 <template>
-  <v-app dark>
+  <v-app id="inspire">
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+      bottom
+      disable-route-watcher
       app
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
     >
+      <v-img
+        :aspect-ratio="16 / 9"
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+      >
+        <v-row align="end" class="lightbox white--text pa-2 fill-height">
+          <v-col>
+            <div class="subheading">小阮</div>
+            <div class="body-1">ruanmaoqi@qq.com</div>
+          </v-col>
+        </v-row>
+      </v-img>
+
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+        <template>
+          <v-list-item link href="https://github.com/rmq767">
+            <v-list-item-action>
+              <v-icon>mdi-github</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>GitHub</v-list-item-title>
+          </v-list-item>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-list-item-title v-on="on">{{
+                  item.title
+                }}</v-list-item-title>
+              </template>
+              <v-img :src="item.image" width="5rem" height="auto"></v-img>
+            </v-tooltip>
+          </v-list-item>
+        </template>
       </v-list>
     </v-navigation-drawer>
+
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
+      color="deep-purple accent-1"
+      hide-on-scroll
+      dark
+      height="54rem"
+      src="https://picsum.photos/1920/1080?random"
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">小阮的个人博客</span>
+      </v-toolbar-title>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      />
     </v-app-bar>
+
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt-child></nuxt-child>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
+
+    <v-bottom-navigation
+      grow
+      color="deep-purple accent-1"
       fixed
+      shift
+      height="3rem"
     >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+      <v-btn append link to="/blog">
+        <span>我的文章</span>
+        <v-icon>mdi-book</v-icon>
+      </v-btn>
+      <v-btn append link to="/message">
+        <span>留言板</span>
+        <v-icon>mdi-file-document-edit</v-icon>
+      </v-btn>
+      <v-btn append link to="/link">
+        <span>友情链接</span>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
+      <v-btn append link to="/life">
+        <span>我的生活</span>
+        <v-icon>mdi-pinwheel</v-icon>
+      </v-btn>
+      <v-btn append link to="/about">
+        <span>Nearby</span>
+        <v-icon>mdi-face</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: "",
+    items: [
+      {
+        icon: "mdi-qqchat",
+        title: "QQ",
+        image: "http://localhost:3000/qq.jpg"
+      },
+      {
+        icon: "mdi-wechat",
+        title: "微信",
+        image: "http://localhost:3000/wechat.png"
+      }
+    ]
+  })
+};
 </script>
---
+++ web/pages/link/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    link
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>

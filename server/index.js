dc2f5ff88d8c8f609e6679e27001a8466f6ffa1f
rmq767 <2501706076@qq.com>
2020-04-10 19:43:58 +0800

    搜索优化

--- server/index.js
+++ server/index.js
@@ -7,24 +7,20 @@ app.use(express.json());
 app.use(require("cors")());
 app.use("/uploads", express.static(__dirname + "/uploads")); //静态文件
 app.use("/admin", express.static(__dirname + "/admin")); //静态文件
-app.use("/", express.static(__dirname + "/dist")); //静态文件
+app.use("/", express.static(__dirname + "/web")); //静态文件
 
 // 中间件处理token
 app.use("/admin/api", async (req, res, next) => {
-  const token = String(req.headers.authorization)
-    .split(" ")
-    .pop();
+  const token = String(req.headers.authorization).split(" ").pop();
   if (token == "undefined") {
     return res.status(401).send({
-      message: "请先登录"
+      message: "请先登录",
     });
   } else {
-    const {
-      id
-    } = jwt.verify(token, app.get("secret"));
+    const { id } = jwt.verify(token, app.get("secret"));
     if (id == "undefined") {
       return res.status(401).send({
-        message: "请先登录"
+        message: "请先登录",
       });
     }
   }
@@ -57,7 +53,7 @@ app.set("secret", "jfdsijf^&T(&UGFU0y80ydsf08;.]s.f");
 // 上传文件接口
 const multer = require("multer");
 const upload = multer({
-  dest: __dirname + "/uploads"
+  dest: __dirname + "/uploads",
 });
 app.post("/admin/api/upload", upload.single("file"), async (req, res) => {
   const file = req.file;
